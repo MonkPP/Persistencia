f282af58c18b3dd9c8ec39d86e47ecfcd0fe908b
MonkPP <pal.monik24@gmail.com>
2023-05-16 20:35:14 -0300

    update

--- cobrancaifmt/src/main/java/edu/ifmt/cobrancaifmt/controller/TituloController.java
+++ cobrancaifmt/src/main/java/edu/ifmt/cobrancaifmt/controller/TituloController.java
@@ -3,7 +3,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
+import edu.ifmt.cobrancaifmt.model.StatusTitulo;
 import edu.ifmt.cobrancaifmt.model.Titulo;
 import edu.ifmt.cobrancaifmt.repository.Titulos;
 
@@ -12,17 +14,22 @@ import edu.ifmt.cobrancaifmt.repository.Titulos;
 public class TituloController {
 	
 	@RequestMapping("/novo")
-	public String novo() {//metodo que reorna uma string com o nome da view
-		return "CadastroTitulo";
+	public ModelAndView novo() {//metodo que reorna uma string com o nome da view
+		ModelAndView mv = new ModelAndView("CadastroTitulo");
+		mv.addObject("todosStatusTitulo", StatusTitulo.values());
+		return mv;
+		
 	}
 	
 	@Autowired //anotação que irá injetar dependencia
 	private Titulos titulos;
 	
 	@RequestMapping(method=RequestMethod.POST)
-	public String salvar(Titulo titulo) {
+	public ModelAndView salvar(Titulo titulo) {
 		titulos.save(titulo);
-		return "CadastroTitulo";
+		ModelAndView mv = new ModelAndView("CadastroTitulo");
+		mv.addObject("mensagem", "Foi salvo com sucesso!");
+		return mv;
 	}
 
 }

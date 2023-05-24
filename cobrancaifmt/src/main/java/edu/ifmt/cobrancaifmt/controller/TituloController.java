a81f3e4719e938eac5ea66b9359b28bd406b0ff6
MonkPP <pal.monik24@gmail.com>
2023-05-23 22:53:44 -0300

    update pesquisatitulo, layout padrao

--- cobrancaifmt/src/main/java/edu/ifmt/cobrancaifmt/controller/TituloController.java
+++ cobrancaifmt/src/main/java/edu/ifmt/cobrancaifmt/controller/TituloController.java
@@ -39,5 +39,11 @@ public class TituloController {
 	public List<StatusTitulo> todosStatusTitulo(){
 		return Arrays.asList(StatusTitulo.values());
 	}
+	
+	@RequestMapping
+	public String pesquisar() {
+		return "PesquisaTitulos";
+	}
+	
 
 }
